6e51c3bff3977f0caf005c51b318eb4e8fb3228b
root <root@ip-172-31-19-79.ap-southeast-1.compute.internal>
2019-02-07 05:25:54 +0000

    genesis commit

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            DICK FACE
           </a>
         </header>
       </div>
